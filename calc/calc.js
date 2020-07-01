e86623a19b4523f38dcc27c5365cb77f85cc8261
Thrasys <tarcisio.word@gmail.com>
2020-07-01 02:33:30 -0300

    exercicios nodejs da rocketseat

---
+++ calc/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./soma");
+var SubFunc = require("./sub");
+var MultFunc = require("./mult");
+var DivFunc = require("./div");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(8,2));
+console.log(MultFunc(3,3));
+console.log(DivFunc(24,2));
---
+++ tipos/Tipos.js
@@ -0,0 +1,65 @@
+//variaveis Comuns
+var nome = "tarcisio";
+let num = 1;
+var isAccepted = true;
+
+//variaveis com Objetos
+var objeto = {};
+objeto.nome = nome;
+objeto.idade = numero;
+
+//Arrays
+var colecao = [];
+colecao[0] = 1;
+colecao[0] = "teste";
+colecao.length()// vai trazer 2 posicoes
+colecao.push()// 
+colecao.forEach(function(item)//percorre todas as posicoes no foreach
+    {
+        console.log(item);
+    })
+
+//Estrutura Condicional
+if (nome = "Jemima") {
+    var
+}
+
+//Declaracao de Funcoes
+function somar(val1, val2){
+    return val1 + val2
+}
+
+objeto.somar = somar;
+objeto.somar(1,2);
+
+if (somar(1,2) ===3) {
+    console.log("funcionou");
+}
+
+//Swicht Case
+switch(numero){
+    case 1: x
+    case 2: x
+    case 3: x
+}
+
+//For
+for(var x=0; x < 10; x++){
+    console.log(0);
+}
+
+//Enquanto
+while (numero < 10) {
+    console.log(numero);
+   numero++; 
+}
+//Caixa alta e baixa
+if (nome.indexOf("1") != -1) {
+    nome.toUpperCase()//caixa alta
+    nome.toLowerCase()//caixa baixa
+}
+//troca texto
+if (nome.indexOf("1") != -1) {
+    var x = parseInt(nome);
+    nome = nome.replace("e" , "f");
+}
